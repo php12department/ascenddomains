f4172fda96ef46b71dcd367fee12b268fb81c74d
php12department <php12.department@gmail.com>
2024-04-18 16:50:49 +0530

    domain list change

--- app/Http/Controllers/DomainController.php
+++ app/Http/Controllers/DomainController.php
@@ -10,8 +10,8 @@ class DomainController extends Controller
 {
     public function index()
     {
-        //$domains = Domain::all();
-        $domains = Domain::take(20)->get();
+        $domains = Domain::all();
+        //$domains = Domain::take(20)->get();
 
         return view('admin.domains.index', ['domains' => $domains]);
         //return view('admin.dashboardbkup', ['domains' => $domains]);
