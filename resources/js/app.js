260329d7999051d57ca7571b41c39a0373e724ad
php12department <php12.department@gmail.com>
2024-07-12 17:58:40 +0530

    js chnages

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 import '../css/app.css';
 import '../css/bootstrap.min.css';
-import './bootstrap.bundle';
+// import './bootstrap.bundle';
 import './jquery.min.js';
 import './popper.min.js';
--- resources/views/layouts/frontjs.blade.php
+++ resources/views/layouts/frontjs.blade.php
@@ -4,5 +4,7 @@
 <script src="{{ asset('assets/js/popper.min.js') }}"></script>
 <script src="https://cdn.datatables.net/2.0.7/js/dataTables.js"></script>
 <script src="https://cdn.datatables.net/2.0.7/js/dataTables.bootstrap5.js"></script>
+<script src="{{ asset('assets/js/bootstrap.bundle.js') }}"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 @yield('scripts')
